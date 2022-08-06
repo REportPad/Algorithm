b34d2420e0bab206847d9ff54f6d2af767a40ff3
jehyun <ggurggur123@naver.com>
2022-08-06 20:31:18 +0900

    Create id46.py

---
+++ Euler/id46.py
@@ -0,0 +1,35 @@
+import math
+
+def isPrime(x):
+    if(x < 0):
+        print('negative')
+        return False
+    if(x == 2 and x == 3 and x == 5 and x == 7): return True
+    if(x%2 == 0): return False
+
+    #9, 11, 13, 15, 17,...
+    sqrt_x = math.sqrt(x)
+    m = int(sqrt_x)
+    for i in range(3,m+1,2):
+        if(x%i == 0):
+            return False
+    
+    return True
+    
+def id46():
+    a = 1
+    x = 35
+
+    while(1):
+        y = x - 2*a*a
+        if(y < 0):
+            print(x)
+            break 
+        elif(isPrime(y)):
+            x = x + 2
+            while(isPrime(x)):
+                x = x + 2
+            a = 1
+        elif(y > 0):
+            a = a + 1
+            
