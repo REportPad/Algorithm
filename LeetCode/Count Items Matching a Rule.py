5309692e409867daca5ae37f18998c2f164bd389
jehyun <ggurggur123@naver.com>
2024-08-18 09:44:16 +0900

    Delete LeetCode/Count Items Matching a Rule.py

--- LeetCode/Count Items Matching a Rule.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
-        thing = {'type':0, 'color':1, 'name':2}
-        key = thing[ruleKey]
-        res =0
-        for item in items:
-            if item[key] == ruleValue:
-                res += 1
-        return res
