e3a628ea209e09030c9ece3401732c76b67ba7fa
jehyun <ggurggur123@naver.com>
2024-08-18 09:44:44 +0900

    Delete LeetCode/Number_of_Arithmetic_Triplets.py

--- LeetCode/Number_of_Arithmetic_Triplets.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
-        ct = 0
-        for i in nums:
-            if (i - diff in nums) and (i + diff in nums):
-                ct += 1
-        return ct
