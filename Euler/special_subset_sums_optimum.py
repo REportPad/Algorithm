2c94be56c8b023fc13163c4c10b80146d6440408
jehyun <ggurggur123@naver.com>
2024-12-30 11:51:07 +0900

    Update special_subset_sums_optimum.py

--- Euler/special_subset_sums_optimum.py
+++ Euler/special_subset_sums_optimum.py
@@ -5,22 +5,22 @@ def is_special_sum_set(s):
     subsets = []
     for i in range(1, len(s) + 1):
         subsets.extend(itertools.combinations(s, i))
-    
+
     # 조건 1: 부분집합의 합이 같으면 안됨
-    subset_sums = {}
+    subset_sums = set()
     for subset in subsets:
-        subset_sum = sum(subset)
-        if subset_sum in subset_sums:
-            return False
-        subset_sums[subset_sum] = subset
-    
+      subset_sum = sum(subset)
+      if subset_sum in subset_sums:
+        return False
+      subset_sums.add(subset_sum)
+
     # 조건 2: 더 큰 부분집합은 항상 더 큰 합을 가짐
     for a, b in itertools.combinations(subsets, 2):
         if len(a) > len(b) and sum(a) <= sum(b):
             return False
         if len(b) > len(a) and sum(b) <= sum(a):
             return False
-    
+
     return True
 
 # n = 7 최적 특수 합집합 찾기
