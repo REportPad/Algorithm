ad73d2ecdc4002070617bdb8df7adab1ffeaf60c
jehyun <ggurggur123@naver.com>
2025-01-07 19:51:04 +0900

    Update square_remainders.py

--- Euler/square_remainders.py
+++ Euler/square_remainders.py
@@ -5,7 +5,8 @@ def square_remainders(N):#16sec
     a2 = a*a
     for n in range(1,2*a+1,2):
       r = (pow(a - 1, n, a2) + pow(a + 1, n, a2)) % a2
-      r_max = max(r_max, r)
+      if r > r_max:
+        r_max = r
     ret += r_max
     # print(a,r_max,ret)
   return ret
